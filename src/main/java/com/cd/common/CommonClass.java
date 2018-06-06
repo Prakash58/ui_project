bcb65de968387df7e5d7e6d05c9a99b528b9fea0
manee2k6 <manee2k6@gmail.com>
2018-06-06 10:12:53 +0530

    Update CommonClass.java

--- src/main/java/com/cd/common/CommonClass.java
+++ src/main/java/com/cd/common/CommonClass.java
@@ -5,8 +5,14 @@ public class CommonClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Welcome Bala for Java project...");
+		//System.out.println("Welcome Bala for Java project...");
+		runProgram();
 
 	}
+	
+	public static void runProgram(){
+		int a=10;
+		int b=20;
+		int sum=a+b;
 
 }
